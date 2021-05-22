0fffee733d3eb2e5b2e2a653878d1a57f82ce9c7
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2021-05-22 18:15:24 +0200

    Update selectors.

--- script/script.js
+++ script/script.js
@@ -2,23 +2,23 @@ const submit = document.querySelector('button[type="submit"]');
 
 submit.addEventListener('click', (e) => {
     e.preventDefault();
-    
-    const name = document.querySelector('label[for="name"]');
-    const surname = document.querySelector('label[for="surname"]');
-    const email = document.querySelector('label[for="email"]');
-    const password = document.querySelector('label[for="password"]');
+
+    const name = document.querySelector('div.name');
+    const surname = document.querySelector('div.surname');
+    const email = document.querySelector('div.email');
+    const password = document.querySelector('div.password');
     let correct = true;
     let fields = [name, surname, email, password];
 
     fields.forEach((el, index) => {
         const input = el.querySelector(':scope input');
         const msg = el.querySelector(':scope .message');
-        const img = el.querySelector(':scope img');
+        const img = el;
 
         const setVisible = (input, msg, img) => {
             input.classList.add('error');
             msg.classList.add('visible');
-            img.classList.add('visible');
+            img.classList.add('error');
         }
 
         if(input.value == '') {
